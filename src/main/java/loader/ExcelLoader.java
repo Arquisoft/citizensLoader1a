90d250f03a6fe0656acae537c4c665bf65f6048c
Danielalba96 <danielalba1996@hotmail.es>
2017-02-13 10:50:56 +0100

    Añadida descripcion del metodo load

--- src/main/java/loader/ExcelLoader.java
+++ src/main/java/loader/ExcelLoader.java
@@ -8,7 +8,14 @@ class ExcelLoader implements FileLoader{
 	
 	@Override
 	public void load(String path) {
-		// TODO Auto-generated method stub		
+		/* TODO Lee el fichero excel de la ruta pasada por parametro
+		 Si el fichero no esta en formato excel, detiene la lectura y escribe en el log
+		 la causa del error.
+		 Va leyendo linea por linea(hay un usuario en cada linea):
+		 Para cada linea crea un objeto User y se lo pasa al metodo cargarDatos del 
+		 ExecuterFacade. Si existe algun fallo de FORMATO se para la lectura y se 
+		 escribe dicho error en el log.
+		 */
 	}	
 	public ExecuterFacade geteF() {
 		return eF;
