bb21106453eeb66dcfb114c2118ba61269516a9d
Daniel Duque Barrientos <UO245553@uniovi.es>
2017-02-14 09:49:40 +0100

    Muestra una ayuda poniendo como parametro --help o no introduciendo ningún parámetro

--- src/main/java/main/LoadUsers.java
+++ src/main/java/main/LoadUsers.java
@@ -18,7 +18,16 @@ public class LoadUsers {
 
 	void run(String... args) {
 		// TODO main para pruebas
-		
-		LoaderSingleton.getInstance().loadFile(args[0]);
+		if(args.length == 0 || args[0].equals("--help")){
+			showHelp();
+		} else{
+			LoaderSingleton.getInstance().loadFile(args[0]);
+		}
+	}
+	
+	private void showHelp(){
+		System.out.println("Ayuda para cargar el archivo a la base de datos:");
+		System.out.println("Introduce como único parámetro la ruta del archivo a cargar.");
+		System.out.println("Para la ayuda introduce como parámetro --help");
 	}
 }
