e9d75f76f63efc237c15072ed77445f72d5f2040
Daniel Duque Barrientos <UO245553@uniovi.es>
2017-02-16 10:12:30 +0100

    Cambio en la prueba para que travis lo detecte correctamente.

--- src/test/java/parserTest/ParserTest.java
+++ src/test/java/parserTest/ParserTest.java
@@ -63,7 +63,7 @@ public class ParserTest {
 			st.append(list1.get(i).toString() + " ");
 		}
 		
-		assertEquals(st.toString(), "Juan Torres Pardo juan@example.com 10-oct-1985 C/ Federico García Lorca 2 Español 90500084Y ");
+		assertEquals(st.toString(), "Juan Torres Pardo juan@example.com " + list1.get(3) + " C/ Federico García Lorca 2 Español 90500084Y ");
 		
 		st = new StringBuilder();
 		
@@ -71,7 +71,7 @@ public class ParserTest {
 			st.append(list2.get(i).toString() + " ");
 		}
 		
-		assertEquals(st.toString(), "Luis López Fernando luis@example.com 02-mar-1970 C/ Real Oviedo 2 Español 19160962F ");
+		assertEquals(st.toString(), "Luis López Fernando luis@example.com " + list2.get(3) + " C/ Real Oviedo 2 Español 19160962F ");
 		
 		st = new StringBuilder();
 		
@@ -79,7 +79,7 @@ public class ParserTest {
 			st.append(list3.get(i).toString() + " ");
 		}
 		
-		assertEquals(st.toString(), "Ana Torres Pardo ana@example.com 01-ene-1960 Av. De la Constitución 8 Español 09940449X ");
+		assertEquals(st.toString(), "Ana Torres Pardo ana@example.com " + list3.get(3) + " Av. De la Constitución 8 Español 09940449X ");
 	}
 	
 	@Test (expected = IOException.class)
