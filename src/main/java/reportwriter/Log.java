450951404862528f1125f1cdbb91a3cec101c47a
Rubén de la Varga <uo246977@uniovi.es>
2017-02-16 17:42:19 +0100

    Añadido ReportWriter

--- src/main/java/reportwriter/Log.java
+++
@@ -1,8 +0,0 @@
-package reportwriter;
-//Esto sería WreportP
-//TODO Buscar informacion sobre este import para hacer el logging
-import java.util.logging.Logger;
-
-public class Log {
-	//TODO hacer el sistema de logging
-}
