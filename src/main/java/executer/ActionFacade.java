4c3d86ca04dd948ae9f1fbae9d56df435104fbe8
Rubén de la Varga <rdlvarcab@gmail.com>
2017-02-20 23:08:40 +0100

    Añadido un test y eliminado método inútil

--- src/main/java/executer/ActionFacade.java
+++ src/main/java/executer/ActionFacade.java
@@ -4,6 +4,5 @@ import model.User;
 
 public interface ActionFacade {
 	public void saveData(User user);
-	public void writeLog(String text);
 	// public void verifySecurity();  Esto es una ampliacion opcional y no se en que consiste
 }
