3c80f46e119018332c06c852e5d369849cf3b470
DanielAlba96 <danielalba1996@hotmail.es>
2017-02-20 23:47:37 +0100

    Bug(otro mas) en los finders

--- src/main/java/persistence/UserFinder.java
+++ src/main/java/persistence/UserFinder.java
@@ -14,42 +14,12 @@ import reportwriter.ReportWriter;
 public class UserFinder {
 
 	public static List<User> findByDNI(String dni) {
-		EntityManager mapper = Jpa.createEntityManager();
-		EntityTransaction trx = mapper.getTransaction();
-		trx.begin();
-		List<User> res = null;
-		try {
-			res = Jpa.getManager().createNamedQuery("User.findByDni", User.class).setParameter(1, dni)
-					.getResultList();
-			trx.commit();
-		} catch (PersistenceException ex) {
-			ReportWriter.getInstance().getWriteReport().log(Level.WARNING, "Error de la BBDD");
-			if (trx.isActive())
-				trx.rollback();
-		} finally {
-			if (mapper.isOpen())
-				mapper.close();
-		}
-		return res;
+		return Jpa.getManager().createNamedQuery("User.findByDni", User.class).
+				setParameter(1, dni).getResultList();
 	}
 
 	public static List<User> findByEmail(String email) {
-		EntityManager mapper = Jpa.createEntityManager();
-		EntityTransaction trx = mapper.getTransaction();
-		trx.begin();
-		List<User> res = null;
-		try {
-			res = Jpa.getManager().createNamedQuery("User.findByEmail", User.class).setParameter(1, email)
-					.getResultList();
-			trx.commit();
-		} catch (PersistenceException ex) {
-			ReportWriter.getInstance().getWriteReport().log(Level.WARNING, "Error de la BBDD");
-			if (trx.isActive())
-				trx.rollback();
-		} finally {
-			if (mapper.isOpen())
-				mapper.close();
-		}
-		return res;
+		return Jpa.getManager().createNamedQuery("User.findByEmail", User.class).
+				setParameter(1, email).getResultList();
 	}
 }
